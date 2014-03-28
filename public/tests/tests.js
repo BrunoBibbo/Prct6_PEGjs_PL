108a209eca26f2a8b511bce71ef9652d8b48f3d1
juanma3108 <alu0100597929@ull.edu.es>
2014-03-28 22:16:59 +0000

    añadido primer test

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -2,9 +2,9 @@ var assert = chai.assert;
  
  suite('Pruebas: ', function() {
     
-    test('Suma: ', function(){
-    object = pl0.parse("x = 12 + 7 .")
-    assert.equal(object.block.st.right.type, "+")
+  test('Resta a izquierdas', function(){  
+    var result = pl0.parse("A = 3 - 4 - 1.");
+    assert.deepEqual(JSON.stringify(result,undefined,2), '{\n  "type": "PROGRAM",\n  "bloque": [\n    [],\n    {\n      "type": "=",\n      "left": {\n        "type": "ID",\n        "value": "A"\n      },\n      "right": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": {\n            "type": "NUM",\n            "value": 3\n          },\n          "right": {\n            "type": "NUM",\n            "value": 4\n          }\n        },\n        "right": {\n          "type": "NUM",\n          "value": 1\n        }\n      }\n    }\n  ]\n}');
   });
 
  });
