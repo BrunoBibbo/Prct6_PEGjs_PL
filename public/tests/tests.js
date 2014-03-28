163033004e92a0c428bb8e1883a6a0ae880e576c
juanma3108 <alu0100597929@ull.edu.es>
2014-03-28 22:53:07 +0000

    añadidos mas tests

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -2,9 +2,68 @@ var assert = chai.assert;
  
  suite('Pruebas: ', function() {
     
+  test('Suma', function(){  
+    var input = pl0.parse("a = 5 + 8 + 2.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
   test('Resta a izquierdas', function(){  
-    var input = pl0.parse("A = 3 - 4 - 1.");
-    assert.equal('[{"type":"=","left":{"type":"ID","value":"A"},"right":{"type":"-","left":{"type":"-","left":{"type":"NUM","value":3},"right":{"type":"NUM","value":4}},"right":{"type":"NUM","value":1}}}]', JSON.stringify(input));
+    var input = pl0.parse("a = 7 - 2 - 5.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('Multiplicación', function(){  
+    var input = pl0.parse("a = 8 * 2 * 5.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('División a izquierdas', function(){  
+    var input = pl0.parse("a = 5 / 7 / 3.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('Preferencia de operador - división', function(){  
+    var input = pl0.parse("a = 4 + 2 / 2.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('Preferencia de operador - multiplicación', function(){  
+    var input = pl0.parse("a = 7 - 1 * 5.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('Call', function(){  
+    var input = pl0.parse("call prueba(24).");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('Program', function(){  
+    var input = pl0.parse("const a=50, b=200;var x,y;z = 10.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('If', function(){  
+    var input = pl0.parse("if a>0 then a=20.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('If - Else', function(){  
+    var input = pl0.parse("if a>0 then a=20 else a=50.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('While', function(){  
+    var input = pl0.parse("while b>0 do b=b+1.");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('Procedure', function(){  
+    var input = pl0.parse("procedure a;var a, v;a = 3;v = 4. ");
+    assert.equal('hola', JSON.stringify(input));
+  });
+
+  test('Error de Sintaxis: ', function(){
+    assert.throws(function() { pl0.parse("a = (5+4."); }, /Expected ")"/);
   });
 
  });
