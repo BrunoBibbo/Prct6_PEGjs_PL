023281a471cc23e009439e4ccb514f7874f97967
juanma3108 <alu0100597929@ull.edu.es>
2014-03-28 22:23:04 +0000

    añadido primer test

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -3,8 +3,8 @@ var assert = chai.assert;
  suite('Pruebas: ', function() {
     
   test('Resta a izquierdas', function(){  
-    var result = pl0.parse("A = 3 - 4 - 1.");
-    assert.Equal(JSON.stringify(result,undefined,2), '{\n  "type": "PROGRAM",\n  "bloque": [\n    [],\n    {\n      "type": "=",\n      "left": {\n        "type": "ID",\n        "value": "A"\n      },\n      "right": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": {\n            "type": "NUM",\n            "value": 3\n          },\n          "right": {\n            "type": "NUM",\n            "value": 4\n          }\n        },\n        "right": {\n          "type": "NUM",\n          "value": 1\n        }\n      }\n    }\n  ]\n}');
+    var input = pl0.parse("A = 3 - 4 - 1.");
+    assert.equal('{\n  "type": "PROGRAM",\n  "bloque": [\n    [],\n    {\n      "type": "=",\n      "left": {\n        "type": "ID",\n        "value": "A"\n      },\n      "right": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": {\n            "type": "NUM",\n            "value": 3\n          },\n          "right": {\n            "type": "NUM",\n            "value": 4\n          }\n        },\n        "right": {\n          "type": "NUM",\n          "value": 1\n        }\n      }\n    }\n  ]\n}', JSON.stringify(input));
   });
 
  });
